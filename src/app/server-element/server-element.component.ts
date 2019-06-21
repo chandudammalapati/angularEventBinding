7b2b8243436e12bb970b16794776bd8679e82c06
RaviChand Dhammalapati <chandudammalapati@gmail.com>
2019-06-20 19:35:54 -0700

    i changing ts file

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, AfterContentInit, AfterContentChecked, ViewChild, ElementRef } from '@angular/core';
 import { item } from '../app.model'
 
 @Component({
@@ -7,10 +7,10 @@ import { item } from '../app.model'
   styleUrls: ['./server-element.component.css'],
   // encapsulation: ViewEncapsulation.Native
 })
-export class ServerElementComponent implements OnInit, OnChanges {
+export class ServerElementComponent implements OnInit, OnChanges, AfterContentInit, AfterContentChecked {
   @Input() element: item;
   @Input() name: string;
-
+  @ViewChild('headingText') headingTextContent : ElementRef;
   
   constructor() { 
     console.log("server element constructer has been initialized.");
@@ -18,6 +18,7 @@ export class ServerElementComponent implements OnInit, OnChanges {
 
   ngOnInit() {
     console.log("ng oninit called in server element");
+    console.log("text heading on init", this.headingTextContent.nativeElement);
   }
 
   ngOnChanges(changes: SimpleChanges){
@@ -25,6 +26,27 @@ export class ServerElementComponent implements OnInit, OnChanges {
   }
   ngDoCheck(): void{
     console.log("ng Do Check called in server element");
+  }
+
+  ngAfterContentInit(): void{
+    console.log("ngAfterContent Init in server element");
+  }
+
+  ngAfterContentChecked(): void{
+    console.log("ngAfterContent Checked in server element");
+  }
+
+  ngAfterViewInit(): void{
+    console.log("ngAfterView Init in server element");
+    console.log("text heading after view init", this.headingTextContent.nativeElement);
+  }
+
+  ngAfterViewChecked(): void{
+    console.log("ngAfterView Checked in server element");
+  }
+
+  ngOnDestroy(): void{
+    console.log("Component destroyed");
   }
 
 }
